964be96d9411564977f6a4dc16aae141f690ed0c
dgottfri <daniel.gottfried@ryerson.ca>
2013-11-28 12:33:02 -0500

    add more requirements
    added show function

--- Lab4/src/imaging.cpp
+++ Lab4/src/imaging.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <string>
+#include <ctime>
 #include "imaging.h"
 
 
@@ -11,16 +12,14 @@ using namespace std;
 
 /***********************IMAGING FUNCTIONS***************************/
 
-imaging :: imaging(){
-	testtaken=0;
-}
+imaging :: imaging(){testtaken=0;}
 
 string imaging :: writecomment(){
 
 	string line;
 	string word;
 
-	cout<<"Type in the desire comments and when finished enter a return then type 'done'\n";
+	cout<<"Type in the desire comments and when finished enter a return then type 'done'\n\n";
 
 	while(word!="done")
 	{
@@ -34,22 +33,7 @@ string imaging :: writecomment(){
 }
 
 
-
-void imaging :: commentPrint(){
-	int i;
-	fstream myfile;
-	string temp;
-
-	myfile.open("imaging_comments.txt");
-
-	for(i=0;i<4;i++){
-	getline (myfile, temp);
-	cout<< temp;
-	}
-
-	myfile.close();
-}
-
+/***********************MRI FUNCTIONS***************************/
 
 void MRI::addcomment(){
 	string temp;
@@ -60,5 +44,86 @@ void MRI::addcomment(){
 	time(&timer);
 	timeinfo= localtime(&timer);
 	comment.append(asctime(timeinfo));
+	comment.append("____________________________________________");
+	comment.append("\n\n");
 }
 
+void MRI::show()
+{
+	if(testtaken==0){
+		cout<<"Test not yet taken"<<endl;
+		cout<<comment<<endl;
+	}
+	else cout<<comment<<endl;
+}
+
+void MRI :: taketest(){
+	testtaken=1;
+
+	//additionally add data through scan of user input or FILE Input Output (FILE I/O)
+	//decision based on data scanning are based on the user of the classes
+}
+
+/***********************CT FUNCTIONS***************************/
+
+void CT::addcomment(){
+	string temp;
+	temp= writecomment();
+	comment.append(temp);
+	time_t timer;
+	struct tm * timeinfo;
+	time(&timer);
+	timeinfo= localtime(&timer);
+	comment.append(asctime(timeinfo));
+	comment.append("____________________________________________");
+	comment.append("\n\n");
+}
+
+void CT::show()
+{
+	if(testtaken==0){
+		cout<<"\n\nNo image scans or data were inputted not yet taken"<<endl;
+		cout<<comment<<endl;
+	}
+	else cout<<comment<<endl;
+	//in addition to comment user can decide what variables to display in table format
+}
+
+void CT :: taketest(){
+	testtaken=1;
+
+	//additionally add data through scan of user input or FILE Input Output (FILE I/O)
+	//decision based on data scanning are based on the user of the classes
+}
+
+/***********************XRAY FUNCTIONS***************************/
+
+void XRAY::addcomment(){
+	string temp;
+	temp= writecomment();
+	comment.append(temp);
+	time_t timer;
+	struct tm * timeinfo;
+	time(&timer);
+	timeinfo= localtime(&timer);
+	comment.append(asctime(timeinfo));
+	comment.append("____________________________________________");
+	comment.append("\n\n");
+}
+
+void XRAY::show()
+{
+	if(testtaken==0){
+		cout<<"Test not yet taken"<<endl;
+		cout<<comment<<endl;
+	}
+	else cout<<comment<<endl;
+	//in addition to comment user can decide what variables to display in table format
+}
+
+void XRAY :: taketest(){
+	testtaken=1;
+
+	//additionally add data through scan of user input or FILE Input Output (FILE I/O)
+	//decision based on data scanning are based on the user of the classes
+}
--- Lab4/src/imaging.h
+++ Lab4/src/imaging.h
@@ -1,12 +1,14 @@
 #ifndef IMAGING_HPP_
 #define IMAGING_HPP_
 
+#include <ctime>
 #include <cstring>
 #include <string>
 
 using namespace std;
 
 class imaging {
+
 protected:
 	string user;
 	string comment;
@@ -14,60 +16,62 @@ protected:
 
 public:
 
-imaging();
-string writecomment();
-void commentPrint();
-void showImage(char*);
+	imaging();
+	string writecomment();
+	void showImage(char*);
 };
 
-class MRI : protected imaging {
+class MRI : public imaging {
 
-string MRIimage;
-string Acquisition_Type;  //method of scanning
-string Scan_Option;  //types of scans
-float Echo_Time;
-float Repetition_Time;
-float Inversion_Time;
-float Image_Frequency;
-int SPP; //Samples per Pixel
-
+	string MRIimage;	//store for MRI file name
+	string Acquisition_Type;  //method of scanning
+	string Scan_Option;  //types of scans
+	int SPP; //Samples per Pixel
+	float Echo_Time;
+	float Repetition_Time;
+	float Inversion_Time;
+	float Image_Frequency;
+	
 
 public:
-void taketest();
-void addcomment();
-void show();
+	//MRI(a,b,...,z) might be used to gather patient scan data info based on the design of hospital data base
+	void taketest();
+	void addcomment();
+	void show();
 };
 
-class CT : protected imaging {
+class CT : public imaging {
 
-string CTimage;
-int Pixel_Representation; //number of pixels
-float Rescale_Slope; //in the equation specified in Rescale Intercept
-float KVP; //Peak kilo voltage output of the x-ray generator used
-int Acquisition_number; //identifying the number for single gathering of data over period of resulted in this image
-float Data_Collection_Diameter; //diameter in mm of the region over which data were collected
-float Exposure_Time;
+	string CTimage;	//store for CT file name
+	int Pixel_Representation; //number of pixels
+	int Acquisition_number; //identifying the number for single gathering of data over period of resulted in this image
+	float Rescale_Slope; //in the equation specified in Rescale Intercept
+	float KVP; //Peak kilo voltage output of the x-ray generator used
+	float Data_Collection_Diameter; //diameter in mm of the region over which data were collected
+	float Exposure_Time;
 
 public:
-void taketest();
-void addcomment();
-void show();
+	//CT(a,b,...,z) might be used to gather patient scan data info based on the design of hospital data base
+	void taketest();
+	void addcomment();
+	void show();
 };
 
-class XRAY : protected imaging {
+class XRAY : public imaging {
 
-string XRAYimage;
-string Image_Type;
-string ARMS; //Anatomic Region Modifier Sequence
-bool Lossy_Image_Compression;
-string Photometric_Interpretation; //Specifies the intended interpretation of the pixel data
-float Reference_Image_Sequence; //gives a co-ordinate of reference
-float Calibration_Image; //Indicates whether a reference object (phantom) of known size is present in the image and was used for calibration
+	string XRAYimage;	//store for XRAY file name
+	string Image_Type;	//basic type/format of the image according to standards
+	string ARMS; //Anatomic Region Modifier Sequence
+	string Photometric_Interpretation; //Specifies the intended interpretation of the pixel data
+	bool Lossy_Image_Compression;	//if compression was performed
+	float Reference_Image_Sequence; //gives a co-ordinate of reference
+	float Calibration_Image; //Indicates whether a reference object (phantom) of known size is present in the image and was used for calibration
 
 public:
-void taketest();
-void addcomment();
-void show();//some peoples incompitance astounds me 
+	//XRAY(a,b,...,z) might be used to gather patient scan data info based on the design of hospital data base
+	void taketest();
+	void addcomment();
+	void show(); 
 };
 
 #endif /* IMAGING_HPP_ */
